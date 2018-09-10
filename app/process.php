8e45ac8880ca85e13251ec4476ae4117eb5009ad
favhyan <favhyan@gmail.com>
2018-09-10 02:12:47 -0500

    added function recursive and sent validation loop

--- app/process.php
+++ app/process.php
@@ -5,7 +5,7 @@ $c = $_GET['c'];
 $x = 0;
 $array = array();
 
-// $array = [4,6];
+// $array = [1,2,3,4];
 
 while ($x<$c) {
 	$num_aleatorio = rand(1,6);
@@ -34,7 +34,58 @@ echo 'Las operaciones para encontrar el resultado: <br>';
 
 echo '<br>***************************************************************<br>';
 
-recorrido($array);	
+recorre($array,$n);
+
+
+function recorre($array,$n,$ite){
+
+		
+	for ($i=0; $i < count($array) ; $i++) { 
+		for ($j=0; $j < count($array); $j++) { 
+			
+			if($i <= $array[$i]){
+				echo $resultadoi = $array[$i];
+				echo ' + ';
+			}
+			if($j <= $array[$j]){
+				echo $resultadoj = $array[$j];
+			}
+
+
+				$valor = $valor+count($resultado);
+			if($resultado < $n){				
+				recor($resultado,$resultadoi,$resultadoj,$n,$valor,$array);
+			}else if($resultado == $n){
+				echo '=';
+				echo $resultado = $resultadoi+$resultadoj;
+				cantidad($valor);
+
+			}
+			echo '<br>';
+		}
+		return $resultado;
+	}
+	
+}
+
+function recor($r,$r1,$r2,$n,$valor,$value){
+
+for ($i=0; $i < count($value) ; $i++) { 
+	# code...
+}
+
+
+				$valor = $valor+count($r);
+				$resultado = $r+$r1;
+				if($r < $n){
+				echo  ' + '.$r1;
+					recor($resultado,$r2,$r1,$n,$valor);
+				}else if($r == $n){
+					echo ' = '.$r;
+				cantidad($valor);
+
+				}
+}
 
 
 
@@ -44,31 +95,4 @@ function cantidad($a){
 
 	</script>';
 }
-
-
-
-function recorrido($arg){
-    $suma = 0;
-    foreach ($arg as $key => $value) {
-        if (is_array($value)){
-            $array2 = $arg[$key];
-             $suma = $suma + recorrido($array2);
-        }else {
-  			
-  			if($suma == 0){
-             	$suma = recorrido($array2) + recorrido($array2);
-              	}else{
-              echo $suma.' + '.$value; 
-
-             $suma = $suma + recorrido($array2);
-echo '<br>';
-         		}
-           $suma = $suma + $value;
-        }
-
-    } 
-    return $suma;
-}
-echo '<br>';
- echo ' = '.$total = recorrido($array);
 
