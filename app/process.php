c4f0024eee1bd5b5917b63bd82bbd0aaad6ffc95
favhyan <FAVHYAN@users.noreply.github.com>
2018-09-10 17:02:03 -0500

    added the function getway

--- app/process.php
+++ app/process.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
+
+// receive variables of get method
+
 $n = $_GET['n'];
 $c = $_GET['c'];
 $x = 0;
 $array = array();
-
-
-
+// create the loop assign values for new array
 while ($x<$c) {
 	$num_aleatorio = rand(1,10);
 	if (!in_array($num_aleatorio,$array)) {
@@ -15,25 +16,13 @@ while ($x<$c) {
 	}
 }
 
-echo '<br>';
-echo '<br>';
-echo '<br>***************************************************************<br>';
-echo '<pre> Los valores que se tomaron para hacer la operación<br>';
-echo '<br>***************************************************************<br>';
-echo '{ ';
-for ($i=0; $i < $c ; $i++) { 
 
-	echo $array[$i].'&nbsp;';
-}
-echo '}'; echo ' ----- Cantidad de posibles ';
+//Method work
+function getWays($c,$n,$array){
 
-recorre($c,$n,$array);
-
-
-function recorre($c,$n,$array){
-
+//assign values to array
  $matrix = array([$n+1],[$c]);
-
+        // first loop and order the values for their sum
         for($j=0;$j<$c;$j++)
 
         {
@@ -41,6 +30,7 @@ function recorre($c,$n,$array){
  	           $matrix[0][$j] = 1;
 
         }
+        //loops nested for assign the solution problem
 
         for($i=1;$i<=$n;$i++)
 
@@ -69,4 +59,19 @@ function recorre($c,$n,$array){
         echo $matrix[$n][$c-1];
 	}
 
+// printer the info on front
+echo '<br>';
+echo '<br>';
+echo '<br>***************************************************************<br>';
+echo '<pre> Los valores que se tomaron para hacer la operación<br>';
+echo '<br>***************************************************************<br>';
+echo '{ ';
+for ($i=0; $i < $c ; $i++) { 
 
+    echo $array[$i].'&nbsp;';
+}
+echo '}'; echo ' ----- Cantidad de posibles ';
+
+
+//Call method process
+getWays($c,$n,$array);
